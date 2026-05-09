fc0b8674b8d67e93fd28a775b03d26646eaf3d4e
Bianca Buzatu <biancabuzatu7@gmail.com>
2026-05-09 14:58:08 +0300

    Am testat compozitia in main

--- src/Modele/Main.java
+++ src/Modele/Main.java
@@ -1,5 +1,7 @@
 package Modele;
 
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
         Persoana p = new Persoana("Ion");
@@ -8,6 +10,8 @@ public class Main {
         Angajat a = new Angajat("Maria", 3000, "Receptioner");
         a.afisare();
         a.gestioneazaRezervare();
+        a.getCardAcces().afisare();
+        System.out.println(a.getCardAcces().oferaAcces());
 
         Administrator admin = new Administrator("Andrei", 5000, "Manager");
         admin.afisare();
@@ -15,5 +19,11 @@ public class Main {
 
         Client c = new Client(1, "Elena", "elena@gmail.com", "0722000000");
         c.afisare();
+
+        Rezervare r = new Rezervare(1, LocalDate.of(2025, 6, 1), LocalDate.of(2025, 6, 5));
+        r.afisare();
+        System.out.println("Pret total: " + r.calculeazaPret(200));
+        r.efectueazaPlata(800, "card");
+        r.afisare();
     }
 }
